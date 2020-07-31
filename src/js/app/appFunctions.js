7a674ff8592651dbfd82c74cd9cab8a548275bfe
Chris Punt <38012621+cpunt@users.noreply.github.com>
2020-07-31 19:17:58 +0100

    Taskframe div only shows up to 25 tasks, tasks re-rendered on scroll.

--- src/js/app/appFunctions.js
+++ src/js/app/appFunctions.js
@@ -1,5 +1,4 @@
 import { daysDiff, weeksDiff, monthsDiff, yearsDiff } from './dateDiff.js';
-import { setDateToTimeframe } from '../sharedFunctions.js';
 
 function getDateIndex(timeframe, startDate, date) {
   let index;
@@ -22,36 +21,4 @@ function getDateIndex(timeframe, startDate, date) {
   return index;
 }
 
-function getTaskDates(timeframe, startDate, endDate) {
-  const taskDates = [];
-  let currentDate = setDateToTimeframe(timeframe, startDate),
-      year,
-      month,
-      day;
-
-  while(currentDate <= endDate) {
-    taskDates.push(currentDate);
-    year = currentDate.getFullYear();
-    month = currentDate.getMonth();
-    day = currentDate.getDate();
-
-    switch(timeframe) {
-      case 'yearly':
-        currentDate = new Date(year + 1, 0);
-        break;
-      case 'monthly':
-        currentDate = new Date(year, month + 1);
-        break;
-      case 'weekly':
-        currentDate = new Date(year, month, day + 7);
-        break;
-      case 'daily':
-        currentDate = new Date(year, month, day + 1);
-        break;
-    }
-  }
-
-  return taskDates;
-}
-
-export { getDateIndex, getTaskDates };
+export { getDateIndex };
